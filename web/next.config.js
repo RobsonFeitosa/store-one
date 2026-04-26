f5e0fdb01819490137857e7a75b25a00725f2cbd
Robson <robson.gw@hotmail.com>
2026-04-26 12:32:50 -0300

    sdf

--- web/next.config.js
+++ web/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   images: {
-    domains: ['localhost', 'store-products-example.s3.amazonaws.com'],
+    domains: ['localhost', 'store-one-rob.duckdns.org', 'store-products-example.s3.amazonaws.com'],
   },
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
   transpilePackages: ['@base-ui/react'],
