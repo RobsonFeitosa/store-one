205ba9ecf593033c34a32c130fe856c848e4f3c3
Robson <robson.gw@hotmail.com>
2026-04-26 13:57:25 -0300

    sdf

--- web/src/pages/_document.page.tsx
+++ web/src/pages/_document.page.tsx
@@ -9,9 +9,54 @@ export default function Document() {
           id="stitches"
           dangerouslySetInnerHTML={{ __html: getCssText() }}
         />
+        {/* Estilo para tentar esconder a barra e o popup do Google Translate */}
+        <style>{`
+          .goog-te-banner-frame.skiptranslate, .goog-te-gadget-icon { display: none !important; }
+          body { top: 0px !important; }
+          .goog-te-menu-value img { display: none !important; }
+          .goog-te-menu-frame { box-shadow: none !important; }
+        `}</style>
       </Head>
       <body>
         <Main />
+        <div id="google_translate_element" style={{ display: 'none' }}></div>
+
+        {/* Scripts do Google Translate */}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            function googleTranslateElementInit() {
+              new google.translate.TranslateElement({
+                pageLanguage: 'pt',
+                includedLanguages: 'en',
+                layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+                autoDisplay: false
+              }, 'google_translate_element');
+            }
+
+            // Script para forçar a tradução se loc=en estiver na URL
+            (function() {
+              const urlParams = new URLSearchParams(window.location.search);
+              if (urlParams.get('loc') === 'en') {
+                var script = document.createElement('script');
+                script.type = 'text/javascript';
+                script.src = '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+                document.body.appendChild(script);
+                
+                // Força o clique na tradução após o carregamento (hack de persistência)
+                window.addEventListener('load', function() {
+                  setTimeout(function() {
+                    var select = document.querySelector('.goog-te-combo');
+                    if (select) {
+                      select.value = 'en';
+                      select.dispatchEvent(new Event('change'));
+                    }
+                  }, 1000);
+                });
+              }
+            })();
+          `
+        }} />
+
         <NextScript />
       </body>
     </Html>
