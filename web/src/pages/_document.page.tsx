ec6a96270448c3d49d1bdd41ddd4283284cebf63
Robson <robson.gw@hotmail.com>
2026-04-26 14:11:25 -0300

    sdafasd

--- web/src/pages/_document.page.tsx
+++ web/src/pages/_document.page.tsx
@@ -9,77 +9,9 @@ export default function Document() {
           id="stitches"
           dangerouslySetInnerHTML={{ __html: getCssText() }}
         />
-        {/* CSS para limpar a interface do Google Translate */}
-        <style>{`
-          .goog-te-banner-frame.skiptranslate { display: none !important; }
-          .goog-te-gadget-icon { display: none !important; }
-          body { top: 0px !important; }
-          .goog-te-menu-value img { display: none !important; }
-          #google_translate_element { display: none !important; }
-          .skiptranslate iframe { display: none !important; }
-        `}</style>
       </Head>
       <body>
         <Main />
-        <div id="google_translate_element"></div>
-
-        <script dangerouslySetInnerHTML={{
-          __html: `
-    function googleTranslateElementInit() {
-      new google.translate.TranslateElement({
-        pageLanguage: 'pt',
-        includedLanguages: 'en',
-        layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
-        autoDisplay: false
-      }, 'google_translate_element');
-    }
-
-    (function() {
-      const urlParams = new URLSearchParams(window.location.search);
-      const isEn = urlParams.get('loc') === 'en' || sessionStorage.getItem('force_en') === 'true';
-
-      if (isEn) {
-        sessionStorage.setItem('force_en', 'true');
-
-        // Carrega o script oficial do Google
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
-        document.body.appendChild(script);
-        
-        // Função agressiva para forçar a tradução
-        function forceTranslate() {
-          const select = document.querySelector('.goog-te-combo');
-          if (select) {
-            if (select.value !== 'en') {
-              select.value = 'en';
-              select.dispatchEvent(new Event('change'));
-            }
-          } else {
-            // Se não achou o seletor ainda, tenta de novo em 200ms
-            setTimeout(forceTranslate, 200);
-          }
-        }
-
-        // Tenta traduzir no load e em intervalos curtos no início
-        window.addEventListener('load', forceTranslate);
-        setTimeout(forceTranslate, 1000); 
-        setTimeout(forceTranslate, 3000);
-
-        // Monitora mudanças de rota (Next.js SPA)
-        let lastUrl = location.href;
-        const observer = new MutationObserver(() => {
-          if (location.href !== lastUrl) {
-            lastUrl = location.href;
-            forceTranslate();
-          }
-        });
-        observer.observe(document, { subtree: true, childList: true });
-      }
-    })();
-  `
-        }} />
-
         <NextScript />
       </body>
     </Html>
